3fdaba8b5b940cd6275a5182f46c06635f3a8d4e
Arseni <yoinezero.a@gmail.com>
2021-09-16 23:17:00 +0300

    small bugfix

--- word_color/get_image_color.py
+++ word_color/get_image_color.py
@@ -8,9 +8,14 @@ def get_color(image: str) -> tuple[int, int, int]:
     count, red, green, blue = 1, 0, 0, 0
     for pixel in pixels:
         count += 1
-        red += pixel[0]
-        green += pixel[1]
-        blue += pixel[2]
+        if type(pixel) != tuple:
+            red += pixel
+            green += pixel
+            blue += pixel
+        else:
+            red += pixel[0]
+            green += pixel[1]
+            blue += pixel[2]
 
     im.close()
     return red // count, green // count, blue // count
